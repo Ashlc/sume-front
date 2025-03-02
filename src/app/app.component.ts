f81ef0857b284289e7ee651a93e8980e32582947
Isadora Paz <mibp@ic.ufal.br>
2025-03-02 17:45:32 -0300

    :sparkles: feat: Update app with new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { tablerPlus } from '@ng-icons/tabler-icons';
+import { TableModule } from 'primeng/table';
+import { IconButtonComponent } from './components/icon-button/icon-button.component';
+import { PageTitleComponent } from './components/page-title/page-title.component';
+import { ProjectDialogComponent } from './components/project-dialog/project-dialog.component';
+import { ProjectTableComponent } from './components/project-table/project-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PageTitleComponent,
+    TableModule,
+    ProjectTableComponent,
+    IconButtonComponent,
+    NgIcon,
+    ProjectDialogComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  viewProviders: [provideIcons({ tablerPlus })],
 })
 export class AppComponent {
-  title = 'sume-front';
+  title = 'Projetos';
+  dialogVisible = false;
+
+  toggleDialog() {
+    this.dialogVisible = !this.dialogVisible;
+  }
+
+  closeDialog() {
+    this.dialogVisible = false;
+  }
 }
