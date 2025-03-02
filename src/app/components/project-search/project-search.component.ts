53f9cc5e4ce85074d50e771a6c5e767bdd43d077
Isadora Paz <mibp@ic.ufal.br>
2025-03-02 17:45:49 -0300

    :construction: feat: Search project component

---
+++ src/app/components/project-search/project-search.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-search',
+  imports: [],
+  templateUrl: './project-search.component.html',
+})
+export class ProjectSearchComponent {}
