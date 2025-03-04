3a4625966e3271f9390a8593ca31085192fe7e34
Isadora Paz <mibp@ic.ufal.br>
2025-03-04 18:29:52 -0300

    :sparkles: feat: Add toast to application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,10 @@ import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-import Aura from '@primeng/themes/aura';
+import { provideToastr } from 'ngx-toastr';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { MyPreset } from './utils/theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +17,10 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: MyPreset,
       },
+    }),
+    provideToastr({
+      positionClass: 'toast-bottom-right',
+      timeOut: 5000,
     }),
   ],
 };
